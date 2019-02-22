9d9e710fcfb63bd86fe311fb8f6c28faf439f1cd
Etienne Gandilhon <etienne.gandilhon@epita.fr>
2019-02-22 15:37:08 +0100

    csv: read the csv

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,2 +1,13 @@
 object Main extends App {
+
+  def getListFromFile(lines: Iterator[String]): List[List[String]] = lines match {
+    case lines if !lines.hasNext => List()
+    case _ => lines.next().split(",").map(_.trim).toList :: getListFromFile(lines)
+  }
+
+  def readCsv(path: String) = {
+    getListFromFile(io.Source.fromFile(path).getLines())
+  }
+
+  println(readCsv("test.csv"))
 }
