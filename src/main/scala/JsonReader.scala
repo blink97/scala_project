65af181daa1fee04a68a91adcb49efd873ed05bc
sidore_m <marc.sidorenko@epita.fr>
2019-02-24 21:17:22 +0100

    Add JsonReader content convert JSON to json object (Argonaut)

--- src/main/scala/JsonReader.scala
+++ src/main/scala/JsonReader.scala
@@ -1,1 +1,24 @@
+/*
+I choose to use a functional oriented library for json parsing
+  Argonaut
+ */
 
+import argonaut._
+import java.io.File
+import scala.io.Source
+
+
+object JsonReader extends App {
+
+  def getFileLines(path: String): Iterator[String] = {
+    Source.fromFile(path).getLines().map(x => x)
+  }
+
+  def getJSONbyMapLines(lines: Iterator[String]): Iterator[Either[String, Json]] = {
+    lines.map(x => Parse.parse(x))
+  }
+
+  val res = getJSONbyMapLines(getFileLines("testJson.json"))
+
+  println(res.next() match {case Right(s) => s case Left(e) => e})
+}
