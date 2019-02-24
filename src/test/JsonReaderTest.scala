5fc6faabed4a5baeb98c08a7e2ba97682ae8f076
sidore_m <marc.sidorenko@epita.fr>
2019-02-24 21:18:17 +0100

    Add test for JsonReader

--- src/test/JsonReaderTest.scala
+++ src/test/JsonReaderTest.scala
@@ -1,1 +1,12 @@
+/*
+TestSection for JsonReader
+ */
+/*
+import JsonReader
 
+object JsonReaderTest extends App {
+  val json1 = Iterator("[{color: \"red\",value: \"#f00\"},{color: \"green\",value: \"#0f0\"},{color: \"blue\",value: \"#00f\"},{color: \"cyan\",value: \"#0ff\"},{color: \"magenta\",value: \"#f0f\"},{color: \"yellow\",value: \"#ff0\"},{color: \"black\",value: \"#000\"}]")
+
+  println(JsonReader.getJSONbyMapLines(json1))
+}
+*/
