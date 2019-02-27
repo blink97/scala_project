4a0babf6b2b4f29452443c2e3cf05d3b7823541a
sidore_m <marc.sidorenko@epita.fr>
2019-02-27 18:28:08 +0100

    Update Json Reader of Dir

--- src/main/scala/JsonReader.scala
+++ src/main/scala/JsonReader.scala
@@ -6,9 +6,18 @@ I choose to use a functional oriented library for json parsing
 import argonaut.Json
 import argonaut.Parse
 import scala.io.Source
-
+import java.io.File
 
 object JsonReader extends App {
+
+  def getListOfFiles(dir: File): List[File] = dir match {
+    case d if d.exists && d.isDirectory => d.listFiles.filter(_.isFile).toList
+    case _ => List[File]()
+  }
+
+  def getJsonFromFiles(list: List[File]): List[Iterator[Either[String, Json]]] = {
+    list.filter(f => ".+\\.json".r.findFirstMatchIn(f.toString).isDefined).map(x => getJSONbyMapLines(getFileLines(x.toString)))
+  }
 
   def getFileLines(path: String): Iterator[String] = {
     Source.fromFile(path).getLines().map(x => x)
@@ -29,6 +38,13 @@ object JsonReader extends App {
     case Left(e) => e
   }
 
+  println("Individual Json Output")
   println(elt1)
   println(elt2)
+
+  // Quick Test dir Jsons
+  // List of Either
+  val resDir = getJsonFromFiles(getListOfFiles(new File("./")))
+  println("(Folder) All Json Files Every Row Output")
+  resDir.foreach(x => x.foreach { case Right(s) => println(s) case Left(e) => println(e) })
 }
