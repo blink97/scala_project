84b513bfc73b056ec535b788c5807ff6763e3450
Dylan Astaing-Peyrat <dylan.astaing-peyrat@epita.fr>
2019-02-27 20:54:24 +0100

    Client's defaults functions done.

--- src/main/scala/WebClient.scala
+++ src/main/scala/WebClient.scala
@@ -2,6 +2,7 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.stream.ActorMaterializer
+import HttpMethods._
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.util.{Failure, Success}
@@ -10,15 +11,51 @@ import scala.util.{Failure, Success}
 
 object WebClient {
 
+  val root : String = "http://localhost:8080/"
+
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
   def main(args: Array[String]): Unit = {
-    implicit val system: ActorSystem = ActorSystem()
-    implicit val materializer: ActorMaterializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
-    implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-    val root : String = "http://localhost:8080/"
+    requestJson()
 
-    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = root + "hello"))
+  }
+
+  def requestJson(id : String = "") : Unit = {
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(GET, uri = root + "/json/" + id))
+
+    responseFuture
+      .onComplete {
+        case Success(res) => println(res)
+        case Failure(_)   => sys.error("something wrong")
+      }
+  }
+
+  def postJson(id : String) : Unit = {
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(POST, uri = root + "/json/" + id))
+
+    responseFuture
+      .onComplete {
+        case Success(res) => println(res)
+        case Failure(_)   => sys.error("something wrong")
+      }
+  }
+
+  def putJson(id : String) : Unit = {
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(PUT, uri = root + "/json/" + id))
+
+    responseFuture
+      .onComplete {
+        case Success(res) => println(res)
+        case Failure(_)   => sys.error("something wrong")
+      }
+  }
+
+  def deleteJson(id : String) : Unit = {
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(DELETE, uri = root + "/json/" + id))
 
     responseFuture
       .onComplete {
