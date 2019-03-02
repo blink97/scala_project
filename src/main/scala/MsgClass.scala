a799f6728a9966d3aabd24a1a60b2a70762bb7c5
sidore_m <marc.sidorenko@epita.fr>
2019-03-02 23:12:33 +0100

    Encode Functions Msg/GeoPos

--- src/main/scala/MsgClass.scala
+++ src/main/scala/MsgClass.scala
@@ -1,12 +1,33 @@
+import argonaut._, Argonaut._
 
 object MsgClass extends App {
+
   case class GeoPos(x: Int, y: Int, alt: Int)
 
   case class Msg(
-                droneId: Int,
-                msgId: Int,
-                msgType: String,
-                temp: Float,
-                geoPos: GeoPos
+                  droneId: Int,
+                  msgId: Int,
+                  msgType: String,
+                  temp: Float,
+                  geoPos: GeoPos
                 )
+
+  implicit def GeoPosEncodeJson: EncodeJson[GeoPos] = {
+    EncodeJson((g: GeoPos) =>
+      ("x" := g.x)
+      ->: ("y" := g.y)
+      ->: ("alt" := g.alt)
+      ->: jEmptyObject
+    )
+  }
+
+  implicit def MsgEncodeJson: EncodeJson[Msg] = {
+    EncodeJson((m: Msg) =>
+      ("droneId" := m.droneId)
+        ->: ("msgId" := m.msgId)
+        ->: ("msgType" := m.msgType)
+        ->: ("temp" := m.temp)
+        ->: ("geoPos" := m.geoPos)
+        ->: jEmptyObject)
+  }
 }
