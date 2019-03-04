4d9d6caa6a67554cf5a151d22cf3bd76caa38df7
Etienne Gandilhon <etienne.gandilhon@epita.fr>
2019-03-04 16:52:59 +0100

    html: adding file with script to create grid

--- build.sbt
+++ build.sbt
@@ -9,5 +9,6 @@ scalaVersion := "2.12.8"
 libraryDependencies ++= Seq(
  "io.argonaut" %% "argonaut" % "6.2.2",
  "com.typesafe.akka" %% "akka-http"   % "10.1.7",
- "com.typesafe.akka" %% "akka-stream" % "2.5.19"
+ "com.typesafe.akka" %% "akka-stream" % "2.5.19", 
+ "org.scala-js" %%% "scalajs-dom" % "0.9.2"
 )
--- src/main/scala/Html.scala
+++ src/main/scala/Html.scala
@@ -1,7 +1,14 @@
+import argonaut.Json
+import org.scalajs.dom
+
 import scala.scalajs.js.JSApp
 
 object Html extends JSApp {
-  def main(): Unit = {
-    println("Bonjour, monde")
+  def createTab(json: Json) = {
+    val array = dom.document.createElement("tr")
+    val cell = dom.document.createElement("td")
+    array.appendChild(cell)
   }
+
+  override def main(): Unit = ???
 }
