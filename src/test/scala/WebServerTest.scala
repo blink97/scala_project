4d266d5475bf8d952f58bc4a52b30ceddb390949
sidore_m <marc.sidorenko@epita.fr>
2019-03-07 12:37:37 +0100

    Add WebServerTest

---
+++ src/test/scala/WebServerTest.scala
@@ -0,0 +1,12 @@
+/*
+1.
+Run WebServer
+2.
+curl -i --form "JsonUpload=@testJson.json" http://localhost:8080/json
+3.
+db/testJson.json appears !!!!
+
+ */
+
+// TODO Test with scala code (Akka Doc has samples)
+
