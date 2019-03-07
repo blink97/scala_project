4e5e364c75c98148ef5cf9209eaf22925fc0f396
Etienne Gandilhon <etienne.gandilhon@epita.fr>
2019-03-07 12:49:25 +0100

    html: testing request in javascipt

--- src/main/scala/Html.scala
+++ src/main/scala/Html.scala
@@ -4,11 +4,17 @@ import org.scalajs.dom
 import scala.scalajs.js.JSApp
 
 object Html extends JSApp {
-  def createTab(json: Json) = {
-    val array = dom.document.createElement("tr")
+  /*def createTab() = {
+    val json = JsonReader.getJSONbyMapLines(JsonReader.getFileLines("testJsonV2.json"))
+      .collect { case Right(value) => value }.next().toString()
+    println(json)
+    /*val array = dom.document.createElement("tr")
     val cell = dom.document.createElement("td")
-    array.appendChild(cell)
+    array.appendChild(cell)*/
+  }*/
+  def main(): Unit = {
+    val json = JsonReader.getJSONbyMapLines(JsonReader.getFileLines("testJsonV2.json"))
+      .collect { case Right(value) => value }.next().toString()
+    println(json)
   }
-
-  override def main(): Unit = ???
 }
