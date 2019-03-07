95dead077c6345f65123cbc741dd5da9ac3b3e2e
sidore_m <marc.sidorenko@epita.fr>
2019-03-07 20:13:42 +0100

    WebServer Entity post

--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -9,8 +9,12 @@ import scala.io.StdIn
 import java.io.File
 import java.nio.file.Paths
 
+import MsgClass.{GeoPos, Msg}
 import akka.http.scaladsl.server.Route
 import akka.stream.scaladsl.FileIO
+import argonaut._
+import Argonaut._
+
 
 import scala.util.{Failure, Success}
 
@@ -26,6 +30,59 @@ object WebServer {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
+    /*
+    entity(as[User]) { user =>
+                  val userCreated: Future[ActionPerformed] =
+                    (userRegistryActor ? CreateUser(user)).mapTo[ActionPerformed]
+                  onSuccess(userCreated) { performed =>
+                    log.info("Created user [{}]: {}", user.name, performed.description)
+                    complete((StatusCodes.Created, performed))
+                  }
+                }
+
+     */
+
+    lazy val routeDB: Route =
+      pathPrefix("msg") {
+        concat(
+          pathEnd {
+            concat(
+              get {
+                /* All msg as JSON */
+                complete(HttpEntity(ContentTypes.`application/json`, ""))
+              },
+              post {
+                entity(as[String]) { s =>
+                  val msgJson = Parse.parse(s)
+                  msgJson match {
+                    case Right(value) => /* Send msg as MSg to DataBase */ complete("OK " + value.toString + " !")
+                    case Left(value) => complete("Error " + value)
+                  }
+                }
+              }
+            )
+          },
+          path(Segment) { id =>
+            concat(
+              get {
+                complete(if (id.toInt >= idMin && id.toInt < 5) {
+                  HttpEntity(ContentTypes.`application/json`, JsonReader
+                    .getJSONbyMapLines(JsonReader.getFileLines("db/" + id + ".json"))
+                    .collect { case Right(value) => value }.next().toString())
+                } else {
+                  HttpEntity(ContentTypes.`text/html(UTF-8)`,
+                    "Error JSON requested don't exist")
+                })
+              },
+              delete {
+                val X = "REmove json or line in json"
+                complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,
+                  "WORK"))
+              }
+            )
+          }
+        )
+      }
 
     /* Warning Need Tests as Lazy */
     lazy val routeDL: Route =
@@ -39,16 +96,12 @@ object WebServer {
                   .collect { case Right(value) => value }.next().toString()))
               },
               post {
-                fileUpload("JsonUpload") {
-                  case (fileInfo, fileStream) =>
-                    val sink = FileIO.toPath(Paths.get("db/") resolve fileInfo.fileName)
-                    val writeResult = fileStream.runWith(sink)
-                    onSuccess(writeResult) { result =>
-                      result.status match {
-                        case Success(_) => complete(s"Successfully written ${result.count} bytes")
-                        case Failure(e) => throw e
-                      }
-                    }
+                entity(as[String]) { s =>
+                  val msgJson = Parse.parse(s)
+                  msgJson match {
+                    case Right(value) => /* Send msg as MSg to DataBase */ complete("OK " + value.toString + " !")
+                    case Left(value) => complete("Error " + value)
+                  }
                 }
               }
             )
