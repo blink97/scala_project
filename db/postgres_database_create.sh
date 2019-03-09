aae3176323f42fb7fa2dff39f829217883459659
sidore_m <marc.sidorenko@epita.fr>
2019-03-09 19:39:19 +0100

    Fix name db to scala-project

--- db/postgres_database_create.sh
+++ db/postgres_database_create.sh
@@ -1,14 +1,13 @@
 
-
-psql postgres -c "CREATE DATABASE test21"
+psql postgres -c "CREATE DATABASE scala-project"
 psql postgres -c "CREATE USER scala WITH ENCRYPTED PASSWORD '42scala'"
-psql postgres -c "GRANT ALL PRIVILEGES ON DATABASE test21 TO scala"
+psql postgres -c "GRANT ALL PRIVILEGES ON DATABASE scala-project TO scala"
 
-# psql postgres -c "\c test21"
+# psql postgres -c "\c scala-project"
 # psql postgres -c "CREATE TABLE MSG(ID INT PRIMARY KEY NOT NULL, DRONE_ID INT NOT NULL, MSG_TYPE CHAR(64) NOT NULL);"
 set -e # Stop script on error
 
-PGPASSWORD=42scala psql -h localhost -U scala test21 <<EOF
+PGPASSWORD=42scala psql -h localhost -U scala scala-project <<EOF
 
 DROP TABLE IF EXISTS msg;
 CREATE TABLE msg (
