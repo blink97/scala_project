4977cafd6fa01c6c3ef2d7a5846d3d4847059ec9
Dylan AP <dylan.astaing-peyrat@epita.fr>
2019-05-07 14:17:50 +0200

    Improved drone demo

--- src/main/scala/Drone.scala
+++ src/main/scala/Drone.scala
@@ -17,54 +17,78 @@ import scala.util.{Failure, Random, Success}
 
 import java.sql.Timestamp
 
-class Drone(drone_id: Int) {
+object Client {
 
-  val id : Int = drone_id
-  var message_id : Int = 0 // TODO : var
+  class Drone(drone_id: Int) {
 
-  val root : String = "http://localhost:8080/"
+    val id : Int = drone_id
+    var message_id : Int = 0 // TODO : var
 
-  implicit val system: ActorSystem = ActorSystem()
-  implicit val materializer: ActorMaterializer = ActorMaterializer()
-  // needed for the future flatMap/onComplete in the end
-  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+    val root : String = "http://localhost:8080/"
 
-  /**
-    *
-    * @return almost random Msg
-    */
-  def generateMsg() : Json = {
+    implicit val system: ActorSystem = ActorSystem()
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-    val randTempD = 5
-    val randTempH = 130
-    val randGeo = 100000
+    /**
+      *
+      * @return almost random Msg
+      */
+    def generateMsg() : Json = {
 
-    val msg = Msg(id, message_id,
-      "'Error'", (Random.nextInt(randTempH) + randTempD).toFloat, "'" + Timestamp.from(Instant.now).toString + "'",
-      GeoPos(Random.nextInt(randGeo), Random.nextInt(randGeo), Random.nextInt(randGeo)))  
+      val randTempD = 5
+      val randTempH = 130
+      val randGeo = 100000
 
-    message_id += 1 // TODO : var
+      val msg = Msg(id, message_id,
+        "'Error'", (Random.nextInt(randTempH) + randTempD).toFloat, "'" + Timestamp.from(Instant.now).toString + "'",
+        GeoPos(Random.nextInt(randGeo), Random.nextInt(randGeo), Random.nextInt(randGeo)))  
 
-    msg.asJson
+      message_id += 1 // TODO : var
+
+      msg.asJson
+    }
+
+    def postJson(json : Json = generateMsg()) : Unit = {
+
+      //val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(POST, uri = root + "msg/" + id, entity = ByteString(json.toString())))
+      val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(POST, uri = root + "msg", entity = ByteString(json.toString())))
+
+      responseFuture
+        .onComplete {
+          case Success(res) => println(res)
+          case Failure(_)   => sys.error("something wrong")
+        }
+    }
   }
-
-  def postJson(json : Json = generateMsg()) : Unit = {
-
-    //val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(POST, uri = root + "msg/" + id, entity = ByteString(json.toString())))
-    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(POST, uri = root + "msg", entity = ByteString(json.toString())))
-
-    responseFuture
-      .onComplete {
-        case Success(res) => println(res)
-        case Failure(_)   => sys.error("something wrong")
-      }
-  }
-}
-
-object demo {
 
   def main(args: Array[String]): Unit = {
-    val drone : Drone = new Drone(3)
-    drone.postJson()
+    val nb_drones = 10
+    val nb_messages = 100
+
+
+    def generateDrones(id : Int = 0) : Unit = {
+
+      val drone : Drone = new Drone(id)
+      calls(drone)
+
+      if (id <= nb_drones)
+        generateDrones(id + 1)
+    }
+
+
+    def calls(drone: Drone, idMessage : Int = 1) : Unit = {
+
+      println("Drone :" + drone.id + " sending message:" + idMessage)
+
+      drone.postJson()
+
+      if (idMessage <= nb_messages)
+        calls(drone, idMessage + 1)
+    }
+
+    generateDrones()
   }
+  
 }
