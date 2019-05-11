7c4c70fef977e037c6d9aefd098f2e611aa85509
sidore_m <marc.sidorenko@epita.fr>
2019-05-11 21:57:20 +0200

    Add data JSON

--- script_gen_data.sh
+++ script_gen_data.sh
@@ -2,8 +2,11 @@
 
 sbt clean compile
 
-for i in {1..50}
-do
-  sbt "runMain DroneDataGenerator $i" &>/dev/null
-  echo "Gen Data Start for $i"
-done
+sbt "runMain DroneDataGenerator 50"
+
+# for i in {1..50}
+# do
+  # sbt "runMain DroneDataGenerator $i" &
+  # echo "Gen Data Start for $i"
+# done
+
