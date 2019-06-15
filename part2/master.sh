5f36f1317b9f11f07c42860b8d1e8bdfd636389e
sidore_m <marc@sidorenko.fr>
2019-06-15 17:28:23 +0200

    Add master script

---
+++ part2/master.sh
@@ -0,0 +1,16 @@
+# !/bin/sh
+
+TIMING_BIG=30
+TIMING_SMALL=5
+
+# Automate Project run
+# Execute all projects script
+./script-zoo-kafka.sh &
+
+sleep $TIMING_BIG
+
+./script-producer.sh
+
+sleep $TIMING_SMALL
+
+./script-consumer.sh
