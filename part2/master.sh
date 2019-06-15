700c0e121fb8d38cdfeac941d9e26807ef9ed5d6
sidore_m <marc@sidorenko.fr>
2019-06-15 19:28:34 +0200

    Update master script

--- part2/master.sh
+++ part2/master.sh
@@ -1,28 +1,41 @@
 #!/bin/sh
 
+LOG_ZOOKEEPER="no"
+
 TIMING_BIG=30
 TIMING_SMALL=5
 SCRIPTS_DIR="scripts"
+PROJECT_JAR="scala_part2_2.12-0.1.jar"
+
+COLOR_BLUE="\e[1m\e[34m"
+COLOR_DEF="\e[39m"
 
 # Automate Project run
 # Execute all projects script
 
 # Package jar
-echo -e "Executing : sbt compile package"
+echo -e "\n$COLOR_BLUE Executing : sbt compile package $COLOR_DEF\n"
 sbt compile package
+cp target/scala-2.12/$PROJECT_JAR .
 
 # zookeeper
-echo -e "Executing : script-zoo-kafka"
-./$SCRIPTS_DIR/script-zoo-kafka.sh
+echo -e "\n$COLOR_BLUE Executing : script-zoo-kafka $COLOR_DEF\n"
+./$SCRIPTS_DIR/script-zoo-kafka.sh $LOG_ZOOKEEPER
 
 sleep $TIMING_BIG
 
 # Producer
-echo -e "Executing : script-producer"
+echo -e "\n$COLOR_BLUE Executing : script-producer $COLOR_DEF\n"
 ./$SCRIPTS_DIR/script-producer.sh
 
 sleep $TIMING_SMALL
 
 # Consumer
-echo -e "Executing : script-consumer"
+echo -e "\n$COLOR_BLUE Executing : script-consumer $COLOR_DEF\n"
 ./$SCRIPTS_DIR/script-consumer.sh
+
+sleep $TIMING_SMALL
+
+# Done
+echo -e "\n$COLOR_BLUE Executions : Done $COLOR_DEF\n"
+
