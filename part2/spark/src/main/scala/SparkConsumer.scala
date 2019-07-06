f6d3fa9b01a44148f8e3035b12de84185c0032df
sidore_m <marc@sidorenko.fr>
2019-07-06 17:43:47 +0200

    Init spark

--- part2/spark/src/main/scala/SparkConsumer.scala
+++ part2/spark/src/main/scala/SparkConsumer.scala
@@ -1,8 +1,11 @@
-
+import spark.implicits._
 
 class SparkConsumor(brokers: String) {
   def process(): Unit = {
-
+    val spark = SparkSession.builder()
+      .appName("spark")
+      .master("local[*]")
+      .getOrCreate()
   }
 }
 
